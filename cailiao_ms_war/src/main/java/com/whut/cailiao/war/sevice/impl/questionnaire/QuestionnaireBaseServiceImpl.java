e98c12287bb75876c14ec7571febf731b006c7e9
niuyang <xfny_hmily@hotmail.com>
2016-02-20 15:49:04 +0800

    lucene base demo
    lucene 简单demo

--- cailiao_ms_war/src/main/java/com/whut/cailiao/war/sevice/impl/questionnaire/QuestionnaireBaseServiceImpl.java
+++ cailiao_ms_war/src/main/java/com/whut/cailiao/war/sevice/impl/questionnaire/QuestionnaireBaseServiceImpl.java
@@ -1,8 +1,25 @@
 package com.whut.cailiao.war.sevice.impl.questionnaire;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field.Store;
+import org.apache.lucene.document.IntField;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TermQuery;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.RAMDirectory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -70,7 +87,49 @@ public class QuestionnaireBaseServiceImpl extends RedisSupport implements Questi
             questionnaire = this.questionnaireDao.selectQuestionnaireById(id);
             setTestData(key, questionnaire);
         }
+        try {
+            createIndex(questionnaire);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return questionnaire;
+    }
+    
+    
+    private void createIndex(Questionnaire questionnaire) throws IOException {
+        
+        if (questionnaire == null) {
+            return;
+        }
+        Analyzer analyzer = new StandardAnalyzer();
+        Directory dir = new RAMDirectory();
+        IndexWriterConfig indexWriterConfig = new IndexWriterConfig(analyzer);
+        IndexWriter indexWriter = new IndexWriter(dir, indexWriterConfig);
+        Document doc = new Document();
+        doc.add(new IntField("id", questionnaire.getId(), Store.YES));
+        doc.add(new StringField("name", questionnaire.getName() + " world", Store.YES));
+        doc.add(new StringField("description", questionnaire.getDescription(), Store.YES));
+        doc.add(new StringField("createPerson", questionnaire.getCreatePerson(), Store.YES));
+        indexWriter.addDocument(doc);
+        indexWriter.close();
+        
+        DirectoryReader ireader = DirectoryReader.open(dir);
+        IndexSearcher searcher = new IndexSearcher(ireader);
+        Term term = new Term("name", "hello");
+        TermQuery query = new TermQuery(term);  
+        TopDocs topdocs = searcher.search(query, 5);  
+        ScoreDoc[] scoreDocs = topdocs.scoreDocs;
+        
+        for(int i=0; i < scoreDocs.length; i++) {  
+            int d = scoreDocs[i].doc;  
+            Document document = searcher.doc(d);  
+            String name = document.get("name");
+            System.out.println("name===="+name);  
+        }  
+        
+        ireader.close();
+        dir.close();
+        
     }
 
 }
