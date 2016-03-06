fd26572461953c696db1b3c5ffbe3b103c467c0c
niuyang <xfny_hmily@hotmail.com>
2016-03-07 02:50:30 +0800

    dubbo配置

--- cailiao_ms_api/src/main/java/com/whut/cailiao/api/service/questionnaire/QuestionnaireService.java
+++ cailiao_ms_api/src/main/java/com/whut/cailiao/api/service/questionnaire/QuestionnaireService.java
@@ -2,7 +2,6 @@ package com.whut.cailiao.api.service.questionnaire;
 
 import com.whut.cailiao.api.commons.ApiResponse;
 import com.whut.cailiao.api.model.questionnaire.QuestionnaireContent;
-import com.whut.cailiao.api.model.questionnaire.QuestionnaireTemplate;
 
 /**
  * Created by niuyang on 16/3/5.
