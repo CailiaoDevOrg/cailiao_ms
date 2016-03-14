1d5fcef0c9ac5aeab977ce1508eb0fa0632cc9ca
Roshine <912627177@qq.com>
2016-03-14 15:20:31 +0800

    创建模板。

--- cailiao_ms_war/src/main/webapp/assets/templateController/createTempController.js
+++ cailiao_ms_war/src/main/webapp/assets/templateController/createTempController.js
@@ -1,8 +1,17 @@
  var createTempApp=angular.module('createTempApp',[]);
  createTempApp.controller('createTempController',function($scope,$http){
- 	//console.log(name);
- 	$scope.submit=function(){
+ 	/*$http.get("",function(response){
+ 		var test=response.createTemplate;
+ 		function setdata(){
+ 			$scope.name=test.name;
+ 			$scope.description=test.description;
+ 			$scope.templateJspUrl=test.templateJspUrl;
+ 			$scope.startTime=test.startTime;
+ 			$scope.endTime=test.endTime;
 
+ 		}
+ 	})*/
+ 	function getdata(){
  		var name=$scope.name;
  		var description=$scope.description;
  		var templateJspUrl=$scope.templateJspUrl;
@@ -15,6 +24,10 @@
  			startTime:startTime,
  			endTime:endTime
  		};
+ 		return createTemplate;
+ 	}
+ 	$scope.submit=function(){
+ 		createTemplate=getdata();
  		console.log(createTemplate);
  	}
  });
