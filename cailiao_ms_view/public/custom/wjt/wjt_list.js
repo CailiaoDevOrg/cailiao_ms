66f470dd74c8cb3273cf340c632a6dacc6f83ee0
niuyang <xfny_hmily@hotmail.com>
2016-04-14 11:07:10 +0800

    table btn的增加问题已经解决

--- cailiao_ms_view/public/custom/wjt/wjt_list.js
+++ cailiao_ms_view/public/custom/wjt/wjt_list.js
@@ -8,6 +8,7 @@
             });
             
             $('#example').DataTable({
+                ordering: false,
                 ajax: {
                     url: '/custom/wjt/list_data.json',
                     dataSrc: 'list'
@@ -17,10 +18,21 @@
                     { data: 'desc' },
                     { data: 'beginTime' },
                     { data: 'endTime' },
-                    { data: 'status' }
+                    { data: 'status' },
+                    { title : 'ops' }
+                ],
+                columnDefs: [
+                    {
+                        targets: -1,
+                        data: 'id',
+                        render: function(data, type, full) {
+                            return '<button class="btn btn-primary btn-xs btn-modify" data-id="' + data + '">修改</button>\
+                                         <button class="btn btn-primary btn-xs btn-delete" data-id="' + data + '">删除</button>';
+                        }
+                    }
                 ]
             });
-            
+
         })();
         
     });
