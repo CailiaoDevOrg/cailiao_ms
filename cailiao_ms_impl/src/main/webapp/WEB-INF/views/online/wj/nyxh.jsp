1246c5b0ae48e18b650157d6e5f9c0609d13507f
StephenNumb <178791827@qq.com>
2016-05-31 11:12:00 +0800

    添加新闻模块表单验证等

--- cailiao_ms_impl/src/main/webapp/WEB-INF/views/online/wj/nyxh.jsp
+++ cailiao_ms_impl/src/main/webapp/WEB-INF/views/online/wj/nyxh.jsp
@@ -7,35 +7,35 @@
 		<div class="row">
 			<div class="col-md-6">
 				<div class="form-group">
-					<label for="nyxh_slfmdh">生料粉末工段电耗</label> 
+					<label for="nyxh_slfmdh">生料粉末工段电耗(kWh/t)</label> 
 					<input type="text" class="form-control" id="nyxh_slfmdh" placeholder="单位：kWh/t">
 				</div>
 				<div class="form-group">
-					<label for="nyxh_kbslmh">可比孰料综合煤耗</label> 
+					<label for="nyxh_kbslmh">可比熟料综合煤耗(kgce/t)</label> 
 					<input type="text" class="form-control" id="nyxh_kbslmh" placeholder="单位：kgce/t">
 				</div>
 				<div class="form-group">
-					<label for="nyxh_kbsldh">可比孰料综合电耗</label> 
+					<label for="nyxh_kbsldh">可比熟料综合电耗(kWh/t)</label> 
 					<input type="text" class="form-control" id="nyxh_kbsldh" placeholder="单位：kWh/t">
 				</div>
 			</div>
 			<div class="col-md-6">
 				<div class="form-group">
-					<label for="nyxh_kbslnh">可比孰料综合能耗</label> 
+					<label for="nyxh_kbslnh">可比熟料综合能耗(kgce/t)</label> 
 					<input type="text" class="form-control" id="nyxh_kbslnh" placeholder="单位：kgce/t">
 				</div>
 				<div class="form-group">
-					<label for="nyxh_kbsndh">可比水泥综合电耗</label> 
+					<label for="nyxh_kbsndh">可比水泥综合电耗(kWh/t)</label> 
 					<input type="text" class="form-control" id="nyxh_kbsndh" placeholder="单位：kWh/t">
 				</div>
 				<div class="form-group">
-					<label for="nyxh_kbsnnh">可比水泥综合能耗</label> 
+					<label for="nyxh_kbsnnh">可比水泥综合能耗(kgce/t)</label> 
 					<input type="text" class="form-control" id="nyxh_kbsnnh" placeholder="单位：kgce/t">
 				</div>
 			</div>
 			<div class="col-md-6">
 				<div class="form-group">
-					<label for="nyxh_dwslfdl">单位熟料余发热电量</label> 
+					<label for="nyxh_dwslfdl">单位熟料余热发电量(kWh/t)</label> 
 					<input type="text" class="form-control" id="nyxh_dwslfdl" placeholder="单位：kWh/t">
 				</div>
 			</div>
--- cailiao_ms_impl/src/main/webapp/assets/custom/online/wj/wj_create.js
+++ cailiao_ms_impl/src/main/webapp/assets/custom/online/wj/wj_create.js
@@ -9,62 +9,71 @@
 
 		$('#commit').on('click', function() {
 			
-			var wjtId = $('#wjtId').val();
-            var data = {
-            	'questionnaireTemplateId': wjtId,
-            	'jsonContent': getWJData()
-            };
-            $.ajax({
-                url: 'online/wj/commit.html',
-                method: 'PUT',
-                data: JSON.stringify(data),
-                contentType: 'application/json',
-                success: function(data) {
-                	data = JSON.parse(data);
-                	if (data.retCode == 200) {
-                        alert('提交成功');
-                        window.close();
-                	} else {
-                		alert('系统忙，请稍后重试');
-                	}
-                },
-                error: function(data) {
-                	alert('网络出现问题，请稍后重试');
-                }
-            });
+			var wjtId = $('#wjtId').val();			
+			if (getWJData(true)) {
+				var data = {
+	                	'questionnaireTemplateId': wjtId,
+	                	'jsonContent': getWJData(true)
+	                };
+				$.ajax({
+	                url: 'online/wj/commit.html',
+	                method: 'PUT',
+	                data: JSON.stringify(data),
+	                contentType: 'application/json',
+	                success: function(data) {
+	                	data = JSON.parse(data);
+	                	if (data.retCode == 200) {
+	                        alert('提交成功');
+	                        window.close();
+	                	} else {
+	                		alert('系统忙，请稍后重试');
+	                	}
+	                },
+	                error: function(data) {
+	                	alert('网络出现问题，请稍后重试');
+	                }
+	            });
+			}
+            
 		});
 
 		$('#submit').on('click', function() {
             var wjtId = $('#wjtId').val();
-            var data = {
-            	'questionnaireTemplateId': wjtId,
-            	'jsonContent': getWJData()
-            };
-            $.ajax({
-                url: 'online/wj/saveTemp.html',
-                method: 'POST',
-                data: JSON.stringify(data),
-                contentType: 'application/json',
-                success: function(data) {
-                	data = JSON.parse(data);
-                	if (data.retCode == 200) {
-                        alert('保存成功');
-                        var wjtId = $('#wjtId').val();
-                        window.location.href = 'online/wj/history/' + wjtId + '.html';
-                	} else {
-                		alert('系统忙，请稍后重试');
-                	}
-                },
-                error: function(data) {
-                	alert('网络出现问题，请稍后重试');
-                }
-            });
+		    var data = {
+				'questionnaireTemplateId' : wjtId,
+				'jsonContent' : getWJData(false)
+			};
+		    $.ajax({
+     			url : 'online/wj/saveTemp.html',
+				method : 'POST',
+				data : JSON.stringify(data),
+				contentType : 'application/json',
+				success : function(data) {
+					data = JSON.parse(data);
+					if (data.retCode == 200) {
+						alert('保存成功');
+						var wjtId = $('#wjtId').val();
+						window.location.href = 'online/wj/history/'
+										+ wjtId + '.html';
+					} else {
+						alert('系统忙，请稍后重试');
+					}
+				},
+				error : function(data) {
+					alert('网络出现问题，请稍后重试');
+				}
+			});
+           
+            
 		});
 
-		function getWJData() {
+		function getWJData(flag) {
+			if (!_getBaseInfoData(flag) || !_getZyxhData(flag)) {
+				return false;
+			}
 			return JSON.stringify({
-				'baseinfo': _getBaseInfoData(),
-			    'zyxh': _getZyxhData(),
+				'baseinfo': _getBaseInfoData(flag),
+			    'zyxh': _getZyxhData(flag),
 			    'nyxh': _getNyxhData(),
 			    'fqpf': _getFqpfData(),
 			    'zysb': _getZysbData()
@@ -73,45 +82,112 @@
 
 		// module 1
 		// 获取基本信息相关数据
-		function _getBaseInfoData() {
+		function _getBaseInfoData(flag) {
+			var qyName = $('#bi_qyName').val();
+			var scLineName = $('#bi_scLineName').val();
+			var scLineScale = $('#bi_scLineScale').val();
+			var qyAddress = $('#bi_qyAddress').val();
+			var linkman = $('#bi_linkman').val();
+			var phoneNumber = $('#bi_phoneNumber').val();
+			if (flag) {
+				if (qyName.trim()=='' || qyName==undefined) {
+					alert("企业名不能为空");
+					return false;
+				} else if (scLineName.trim()=='' || scLineName==undefined) {
+					alert("生产线名称不能为空");
+					return false;
+				} else if (scLineScale.trim()=='' || scLineScale==undefined) {
+					alert("生产线规模不能为空");
+					return false;
+				} else if (qyAddress.trim()=='' || qyAddress==undefined) {
+					alert("企业地址不能为空");
+					return false;
+				} else {
+					return {
+						'bi_qyName': qyName,
+						'bi_scLineName': scLineName,
+						'bi_scLineScale': scLineScale,
+						'bi_qyAddress': qyAddress,
+						'bi_linkman': linkman,
+						'bi_phoneNumber': phoneNumber
+					}
+				}
+				
+			}
 			return {
-				'bi_qyName': $('#bi_qyName').val(),
-				'bi_scLineName': $('#bi_scLineName').val(),
-				'bi_scLineScale': $('#bi_scLineScale').val(),
-				'bi_qyAddress': $('#bi_qyAddress').val(),
-				'bi_linkman': $('#bi_linkman').val(),
-				'bi_phoneNumber': $('#bi_phoneNumber').val()
+				'bi_qyName': qyName,
+				'bi_scLineName': scLineName,
+				'bi_scLineScale': scLineScale,
+				'bi_qyAddress': qyAddress,
+				'bi_linkman': linkman,
+				'bi_phoneNumber': phoneNumber
 			}
 		}
 
 		// module 2
 		// 获取资源消耗相关数据
-		function _getZyxhData() {
-			return {
-				'slTable': __getSlTable(),
-				'rlTable': __getRlTable(),
-				'zyxh_slcl': $('#zyxh_slcl').val(),
-				'snTable': __getSNTable(),
-				'zyxh_sncl': $('#zyxh_sncl').val()
+		function _getZyxhData(flag) {
+			if (!__getSlTable(flag) || !__getRlTable(flag) || !__getSNTable(flag)) {
+				return false;
+			} else {
+				return {
+					'slTable': __getSlTable(flag),
+					'rlTable': __getRlTable(flag),
+					'zyxh_slcl': $('#zyxh_slcl').val(),
+					'snTable': __getSNTable(flag),
+					'zyxh_sncl': $('#zyxh_sncl').val()
+				}
 			}
+			
 		}
 
 		// 获取熟料表格数据
-		function __getSlTable() {
+		function __getSlTable(flag) {
 			var slTable = [];
 			for (var i = 1; i <= 6; i++) {
-				slTable.push({
-					'zyxh_sl_yclName': $('#zyxh_sl_yclName_' + i).val(),
-					'zyxh_sl_yclXhqk': $('#zyxh_sl_yclXhqk_' + i).val()
-				});
+				if (flag) {
+					if ($('#zyxh_sl_yclName_' + i).val() != ''
+							&& $('#zyxh_sl_yclXhqk_' + i).val() == ''
+							|| $('#zyxh_sl_yclName_' + i).val() == ''
+							&& $('#zyxh_sl_yclXhqk_' + i).val() != '') {
+						alert("熟料烧成原材料名称和消耗情况必须保持一致！");
+						//break;
+						return false;
+					} else {
+						slTable.push({
+							'zyxh_sl_yclName': $('#zyxh_sl_yclName_' + i).val(),
+							'zyxh_sl_yclXhqk': $('#zyxh_sl_yclXhqk_' + i).val()
+						});
+					}
+				} else {
+					slTable.push({
+						'zyxh_sl_yclName': $('#zyxh_sl_yclName_' + i).val(),
+						'zyxh_sl_yclXhqk': $('#zyxh_sl_yclXhqk_' + i).val()
+					});
+				}
+				
 			}
 			return slTable;
 		}
 
 		// 获取燃料表格数据
-		function __getRlTable() {
+		function __getRlTable(flag) {
 			var rlTable = [];
 			for (var i = 1; i <= 3; i++) {
+				if (flag) {
+					if ($('#zyxh_rl_yclName_' + i).val() == ''
+						&& $('#zyxh_rl_yclXhqk_' + i).val() != ''
+							|| $('#zyxh_rl_yclName_' + i).val() != ''
+								&& $('#zyxh_rl_yclXhqk_' + i).val() == '') {
+						alert("燃料的原材料名称和消耗情况必须一致！");
+						return false;
+					} else {
+						rlTable.push({
+							'zyxh_rl_yclName': $('#zyxh_rl_yclName_' + i).val(),
+							'zyxh_rl_yclXhqk': $('#zyxh_rl_yclXhqk_' + i).val()
+						});
+					}
+				}
 				rlTable.push({
 					'zyxh_rl_yclName': $('#zyxh_rl_yclName_' + i).val(),
 					'zyxh_rl_yclXhqk': $('#zyxh_rl_yclXhqk_' + i).val()
@@ -121,9 +197,18 @@
 		}
 
 	    // 获取水泥表格
-	    function __getSNTable() {
+	    function __getSNTable(flag) {
 	    	var snTable = [];
 	    	for (var i = 1; i <= 10; i++) {
+	    		if (flag) {
+	    			if ($('#zyxh_sn_yclName_' + i).val() == ''
+	    				&& $('#zyxh_sn_yclXhqk_' + i).val() != ''
+	    					|| $('#zyxh_sn_yclName_' + i).val() != ''
+	    						&& $('#zyxh_sn_yclXhqk_' + i).val() == '') {
+	    				alert("水泥制成用原材料的名称和消耗情况必须一致！");
+	    				return false;
+	    			}
+	    		}
 	    		snTable.push({
 	    			'zyxh_sn_yclName': $('#zyxh_sn_yclName_' + i).val(),
 	    			'zyxh_sn_yclXhqk': $('#zyxh_sn_yclXhqk_' + i).val()
@@ -134,7 +219,22 @@
 
 	    // module 3 
 	    // 获取能源消耗数据
-	    function _getNyxhData() {
+	    function _getNyxhData(flag) {
+	    	if (flag) {
+	    		if ($('#nyxh_slfmdh').val().trim()=='' || $('#nyxh_slfmdh').val() == undefined) {
+	    			alert("生料粉末工段电耗不能为空");
+	    			return false;
+	    		} else if ($('#nyxh_kbslmh').val().trim() == '' || $('#nyxh_kbslmh').val() == undefined) {
+	    			alert("可比熟料综合煤耗不能为空");
+	    			return false;
+	    		} else if ($('#nyxh_kbsldh').val().trim() == '' || $('#nyxh_kbsldh').val() == undefined) {
+	    			alert("可比熟料综合电耗不能为空");
+	    			return false;
+	    		} else if ($('#nyxh_kbslnh').val().trim() == '' || $('#nyxh_kbslnh').val() == undefined) {
+	    			alert("可比熟料综合能耗不能为空");
+	    			return false;
+	    		}
+	    	}
 	    	return {
 	    		'nyxh_slfmdh': $('#nyxh_slfmdh').val(),
 	    		'nyxh_kbslmh': $('#nyxh_kbslmh').val(),
